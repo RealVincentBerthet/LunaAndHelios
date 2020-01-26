f47f2411057092c6d9be501b0136b5b6f01fe21a
PandoS0CE <33762280+PandoS0CE@users.noreply.github.com>
2020-01-26 11:37:51 +0100

    ajout musique theme (#15)

---
+++ Assets/Scripts/themeSong.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class themeSong : MonoBehaviour
+{
+    private static themeSong instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
