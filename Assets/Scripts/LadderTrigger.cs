5413f98d12a1d1979780cc77812f962ab400b776
matthias <matthiaspinard@gmail.com>
2020-01-26 12:01:12 +0100

    debut lv 3

---
+++ Assets/Scripts/LadderTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderTrigger : MonoBehaviour
+{
+    private bool onLadder = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerController playerController = collision.GetComponent<PlayerController>();
+        if(playerController != null)
+        {
+            onLadder = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerController playerController = collision.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            onLadder = false;
+        }
+    }
+}
