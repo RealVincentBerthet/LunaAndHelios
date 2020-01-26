8f99daf515c9b3801675b3822ae8476905968de4
matthias <matthiaspinard@gmail.com>
2020-01-26 12:46:21 +0100

    save lv3

--- Assets/Scripts/LadderTrigger.cs
+++ Assets/Scripts/LadderTrigger.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class LadderTrigger : MonoBehaviour
 {
     private bool onLadder = false;
+    private bool isClimbing;
+    private List<Collider2D> m_collide;
+
+    public void Awake()
+    {
+        m_collide = new List<Collider2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,24 +21,28 @@ public class LadderTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isClimbing = Input.GetKey(KeyCode.E);
+    }
+
+    public void FixedUpdate()
+    {
         
     }
 
-    public void OnTriggerStay2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        PlayerController playerController = collision.GetComponent<PlayerController>();
-        if(playerController != null)
+        if (!m_collide.Contains(collision))
         {
-            onLadder = true;
+            m_collide.Add(collision);
         }
     }
 
+
+
     public void OnTriggerExit2D(Collider2D collision)
     {
-        PlayerController playerController = collision.GetComponent<PlayerController>();
-        if (playerController != null)
-        {
-            onLadder = false;
-        }
+        m_collide.Remove(collision);
     }
+
+
 }
